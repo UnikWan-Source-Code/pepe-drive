fa2495f81acd9d449290bc2a3a614ed34c35511b
babuTech <baburao@unikwan.com>
2023-02-22 15:51:08 +0530

    mint changes done

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import Link from "next/link";
 
 export default function Home() {
   const [showSound, setSound] = useState(false);
-  const [showWallet, setWallet] = useState(false);  
+  const [showWallet, setWallet] = useState(false);
   const [showMint, setMint] = useState(false);
   const [showConnection, setConnection] = useState("");
   const [showNum, setNum] = useState(0);
@@ -30,18 +30,21 @@ export default function Home() {
   }
 
   const mintNow = () => {
-    setConnection("minting");
-    setTimeout(function () {
-      setConnection("minted");
-      setMint(true);
-    }, 2000);
+    if (showNum > 0) {
+      setConnection("minting");
+      setTimeout(function () {
+        setConnection("minted");
+        setMint(true);
+      }, 2000);
+    }
+
   }
 
   const togglePlay = () => {
     var audio = document.getElementById("audio");
-    if(audio.paused) {
+    if (audio.paused) {
       setSound(true);
-      audio.play();      
+      audio.play();
     }
     else {
       setSound(false);
@@ -51,21 +54,17 @@ export default function Home() {
   }
 
   const incCount = () => {
-    var count = document.getElementById("count").innerHTML;
-    if(count >= 0) {
-      setNum(parseInt(count) + 1);
-    }
+    setNum(showNum + 1);
   }
 
   const decCount = () => {
-    var count = document.getElementById("count").innerHTML;
-    if(count > 0) {
-      setNum(parseInt(count) - 1);
+    if (showNum != 0) {
+      setNum(showNum - 1);
     }
   }
 
   return (
-    <>     
+    <>
       <main className={styles.pdrive_landing_page}>
         <div className={styles.pdrive_hero_section}>
           <div className={styles.pdrive_page_top}>
@@ -84,22 +83,22 @@ export default function Home() {
                 <article className={`${styles.cnt_walet_dtails} ${styles.cnt_walet_dtails_mob} ${styles.font_style_1}`}>
                   ETH: $1689 | GAS: 34 GWAI
                 </article>
-                {showWallet == false
-                ?
-                  <a href="" onClick={(e) => {connectWallet(e)} } className={`${styles.cnt_walet_text} ${styles.font_style_1}`}>                
+                {!showWallet
+                  ?
+                  <a href="" onClick={(e) => { connectWallet(e) }} className={`${styles.cnt_walet_text} ${styles.font_style_1}`}>
                     CONNECT WALLET
                   </a>
-                :
+                  :
                   <>
-                  <article className={`${styles.cnt_walet_text} ${styles.font_style_1}`}>WALLET CONNECTED</article>
-                  <article className={styles.cnt_wallet_adr}>0x29...C938</article>
+                    <article className={`${styles.cnt_walet_text} ${styles.font_style_1}`}>WALLET CONNECTED</article>
+                    <article className={styles.cnt_wallet_adr}>0x29...C938</article>
                   </>
-                }                
+                }
               </div>
             </div>
             <div className={`${styles.eth_details_box} ${styles.eth_details_box_desk}`}>
               <article className={`${styles.cnt_walet_dtails} ${styles.font_style_1}`}>
-                ETH: $1689 | GAS: 34 GWAI
+                ETH: {showWallet ? '$1689' : '$0000'} | GAS: {showWallet ? '34' : '00'} GWAI
               </article>
             </div>
           </div>
@@ -113,82 +112,82 @@ export default function Home() {
             />
             <div className={styles.pdrive_mint_steps}>
               {showMint == true
-              ?
+                ?
                 <article className={`${styles.font_style_1} ${styles.extra_lh}`}>
-                  YOU SUCCESSFULLY<br/>
-                  MINTED X PEPE DRIVE NFTS
+                  YOU SUCCESSFULLY<br />
+                  MINTED {showNum} PEPE DRIVE NFTS
                 </article>
-              :
-              showWallet == true
-              ?
-                <>
-                <div className={`${styles.mb} ${styles.count_flex}`}>
-                  <div className={styles.arrow_flex}>
-                    <span onClick={() => {incCount()} }>&#8710;</span>
-                    <span onClick={() => {decCount()} }>&#8711;</span>
-                  </div>
+                :
+                showWallet == true
+                  ?
+                  <>
+                    <div className={`${styles.mb} ${styles.count_flex}`}>
+                      <div className={styles.arrow_flex}>
+                        <span onClick={() => { incCount() }}>&#8710;</span>
+                        <span onClick={() => { decCount() }}>&#8711;</span>
+                      </div>
+                      <article className={`${styles.font_style_1}`}>
+                        <span id="count">{showNum ? showNum : 'x'}</span> <span className={styles.mint_btn} onClick={() => { mintNow() }}>MINT NOW</span> 0.24 ETH
+                      </article>
+                    </div>
+                    <div>
+                      <article className={`${styles.font_style_1}`}>
+                        37 / 4200 AVAILABLE
+                      </article>
+                    </div>
+                  </>
+                  :
                   <article className={`${styles.font_style_1}`}>
-                    <span id="count">{showNum}</span>x <span className={styles.mint_btn} onClick={() => {mintNow()} }>MINT NOW</span> 0.24 ETH
+                    SOON... FEW..
                   </article>
-                </div>
-                <div>
-                  <article className={`${styles.font_style_1}`}>
-                    37 / 4200 AVAILABLE
-                  </article>
-                </div>
-                </>                
-              :
-                <article className={`${styles.font_style_1}`}>
-                  SOON... FEW..
-                </article>
               }
             </div>
           </div>
           <div className={styles.pdrive_page_bottom}>
             <div className={styles.pdrive_page_sound}>
               <audio id="audio" src="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3" preload="auto"></audio>
-              <Link href="/" onClick={(e) => {e.preventDefault(); togglePlay()} }>
-              <Image
-                src={showSound == true ? soundOn : soundOff}
-                alt="Logo"
-                width={80}
-                height={80}
-              />
+              <Link href="/" onClick={(e) => { e.preventDefault(); togglePlay() }}>
+                <Image
+                  src={showSound == true ? soundOn : soundOff}
+                  alt="Logo"
+                  width={80}
+                  height={80}
+                />
               </Link>
             </div>
             <div className={styles.pdrive_mint_status}>
               {showConnection == "minting"
-              ?
+                ?
                 <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
                   MINTING...
                 </article>
-              :
-              showConnection == "minted"
-              ?
-                <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
-                  MINTED
-                </article>
-              :
-              showConnection == "connecting"
-              ?
-                <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
-                  CONNECTING...
-                </article>
-              :
-              showConnection == "connected"
-              ?
-                <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
-                  CONNECTED
-                </article>
-              :
-                <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
-                  X DAYS TO MINT
-                </article>
-              }              
+                :
+                showConnection == "minted"
+                  ?
+                  <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
+                    MINTED
+                  </article>
+                  :
+                  showConnection == "connecting"
+                    ?
+                    <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
+                      CONNECTING...
+                    </article>
+                    :
+                    showConnection == "connected"
+                      ?
+                      <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
+                        CONNECTED
+                      </article>
+                      :
+                      <article className={`${styles.mint_status_text} ${styles.font_style_1}`}>
+                        X DAYS TO MINT
+                      </article>
+              }
               <ProgressBar className={"progressbar_border"} barContainerClassName={(showConnection == "connected" || showConnection == "minted") ? "progressbar_inner_bg" : "progressbar_width"} completed={(showConnection == "connecting" || showConnection == "minting") ? 100 : (showConnection == "connected" || showConnection == "minted") ? 0 : 0} transitionDuration={(showConnection == "connected" || showConnection == "minted") ? "0s" : "2s"} bgColor={"#70B959"} borderRadius={"0"} />
             </div>
             <div className={styles.pdrive_page_social}>
-              <Link href="">
+              <Link target='_blank' href="https://twitter.com/">
                 <Image
                   src={twitterLogo}
                   alt="Logo"
