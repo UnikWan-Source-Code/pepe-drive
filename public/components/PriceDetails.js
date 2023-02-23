738a6f1a56f1e958a774af35e11f44d9e9cfe07b
babuTech <baburao@unikwan.com>
2023-02-24 00:33:15 +0530

    blur changes

--- public/components/PriceDetails.js
+++ public/components/PriceDetails.js
@@ -1,6 +1,7 @@
-const PriceDetails = () => {
+export default function PriceDetails() {
     return (
-        <div>yes</div>
+        <article className={`${styles.cnt_walet_dtails} ${styles.font_style_1}`}>
+            ETH: {showWallet ? '$1689' : '$0000'} | GAS: {showWallet ? '34' : '00'} GWEI
+        </article>
     )
-}
-export default PriceDetails;
+} 
