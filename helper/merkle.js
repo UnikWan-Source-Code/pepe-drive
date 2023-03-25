daf8b6e46d813fae173402455c9e0b84da75e7e2
Andreas <andreas.berghammer@gmx.de>
2023-03-25 18:45:21 +0100

    WL Update

--- helper/merkle.js
+++ helper/merkle.js
@@ -10,10 +10,10 @@ const leaves = whitelist.map((x) => keccak256(x));
 
 const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 const root = tree.getHexRoot();
-const leaf = keccak256('0xA65C631C42D623D2BF4791D0EE3dcddba27a6872');
+const leaf = keccak256('0x0A346Ce069Ad29715e08B34913Ff4eCe9d978c2e');
 const proof = tree.getHexProof(leaf);
 
 console.log(root); //0xe4d077bb973063a680d206b8db2614982f70bb613c8330b1e75608c6c2570a97
 console.log(leaf);
 console.log(proof);
-console.log(tree.toString());
+// console.log(tree.toString());
